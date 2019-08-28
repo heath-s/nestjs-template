ace67d1086357fc59b4a40d378698b98ce4255cf
신희성 (SINN Huiseong) <sinn0h@ncsoft.com>
2019-08-28 13:43:47 +0900

    [change] AppController: 시험 코드 comment로 변경 [add] AuditLogController https://jira.ncsoft.com/browse/SECSYS-176

--- src/audit-log/audit-log.controller.ts
+++ src/audit-log/audit-log.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Like, Repository } from 'typeorm';
+
+import { AuditLog } from './audit-log.entity';
+import { Pagination } from '../shared/pagination.decorator';
 
 @Controller('audit-log')
-export class AuditLogController {}
+export class AuditLogController {
+  constructor(
+    @InjectRepository(AuditLog, 'AuditLogConnection')
+    private readonly auditLogRepo: Repository<AuditLog>,
+  ) { }
+
+  @Get(':id')
+  async getAuditLogItem(@Param('id') id: number) {
+    return await this.auditLogRepo.findOne(id);
+  }
+
+  @Get('')
+  async getAuditLogList(
+    @Pagination(20) { skip, take },
+    @Query('search-by') by?: string, @Query('search-keyword') keyword?: string,
+  ) {
+    const where = {};
+    if (by && keyword) {
+      where[by] = by === 'nickname' ? Like(`%${keyword}%`) : keyword;
+    }
+
+    const [logs, count] = await this.auditLogRepo.findAndCount({
+      select: ['id', 'context', 'error', 'nickname', 'username', 'createdAt'],
+      where,
+      order: { createdAt: 'DESC' },
+      skip,
+      take,
+    });
+    return { count, logs };
+  }
+}
