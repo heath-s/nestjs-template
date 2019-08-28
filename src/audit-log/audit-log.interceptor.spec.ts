ee8f38a590fdd6ff27b739c9914ba7b4293597d3
신희성 (SINN Huiseong) <sinn0h@ncsoft.com>
2019-08-28 11:34:00 +0900

    [remove] Test codes https://jira.ncsoft.com/browse/SECSYS-176

--- src/audit-log/audit-log.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuditLogInterceptor } from './audit-log.interceptor';
-
-describe('AuditLogInterceptor', () => {
-  it('should be defined', () => {
-    expect(new AuditLogInterceptor()).toBeDefined();
-  });
-});
--- src/auth/auth-exception.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthExceptionFilter } from './auth-exception.filter';
-
-describe('AuthExceptionFilter', () => {
-  it('should be defined', () => {
-    expect(new AuthExceptionFilter()).toBeDefined();
-  });
-});
