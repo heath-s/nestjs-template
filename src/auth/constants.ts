06176858c1950111e461989865ac3b625bbe234d
신희성 (SINN Huiseong) <sinn0h@ncsoft.com>
2019-09-03 11:40:13 +0900

    [fix] Auth constants: Public Key 제거

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,22 +1,8 @@
 export default {
   COOKIE_NAME: 'JWTSecuritySystemTeam',
   KERBEROS_DOMAIN: 'kerberos.ncsoft.com',
-  PUBLIC_KEY: `
------BEGIN PUBLIC KEY-----
-MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAqvODgLG7kIHfzim/5wKW
-G7HnImUAb7/ylyfuYqY2zB5HHkzneiDc9b3IDsc0vnP+/fMG5YXAv4vaXGWxaZhX
-2fqEMIZgDAd70LmcfxQgTDvYpojn+7GkudiJu8WkhgDJWGKltBvMXbXuAHd8+rzU
-Roxnb+sZiwtgFTK66Jq8rCLtl8OArhaS00pDDu1d6AxuTm6aw28PkjMKalRgPA2U
-LDNrwNZEkHeMnTwY5xTGOWOOM5RzpfiScEjWIEEDWGcSXKzsqDFpzaXIIUjOP3iW
-tZmsoV6juiTTX6cFBHcODdbfqHjlV2uBk8+LqK4senApz9ixJN6Mg3+OrREmYLtC
-5wEPZYLjUvR8vjFk9nQV6RPgBugEpLX5ciMjJvgy72to2cmxRlqlle9z0/U3kqJM
-3LegZ1HtmM1sbpXQE6gHqttWRSbehiBiqS0DZEFsv1fZVI593PGi20QS4cAwMscH
-cYsviEqFEC82Zr7c+BntHeOk4L+juV1/nS5boR2A9JQA+lNZ39W2GMPvBj60swvn
-YRQwgPsEHok6+NYmJMJyL3rf7+RxiPiDFregRLv87Lh6PxRokyqDHDfDx1AONEGm
-TxdCe/j3uXMQC9ahrbfIaE/6TIY/GgVkIMf00JJmBB0hoXLYXio86mytv3roYTla
-iuzqXiGtMEmaqU5Mspx76gcCAwEAAQ==
------END PUBLIC KEY-----
-  `,
+  // TODO: JWT Encryption Public Key
+  PUBLIC_KEY: 'PUBLIC_KEY',
   // TODO: JWT Service Name
   SERVICE_NAME: 'SERVICE_NAME',
 };
