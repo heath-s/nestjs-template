66421e69ee6f788c02a15577f5f5bc20ec6c378e
신희성 (SINN Huiseong) <sinn0h@ncsoft.com>
2019-09-20 09:03:12 +0900

    [fix] main.ts: trust proxy, x-powered-by 설정 https://jira.ncsoft.com/browse/SECSYS-192

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import environment from './environment';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('x-powered-by', false);
+  app.set('trust proxy', true);
   app.use(cookieParser());
   await app.listen(environment.port);
 }
